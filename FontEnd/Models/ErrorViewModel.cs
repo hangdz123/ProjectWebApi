ffbe9410c1d0372a2ba882f12dc68eb9e8a4c29f
hangdz123 <emperor2002fix@gmail.com>
2024-08-17 09:52:11 +0700

    add

---
+++ FontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
